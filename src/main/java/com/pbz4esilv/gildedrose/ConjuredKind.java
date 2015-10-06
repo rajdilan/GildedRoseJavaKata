b9662e13e57ab29de212347ec4aecc770a012ac2
Dilan <dilan.rajaratnam@devinci.fr>
2015-10-06 16:45:41 +0200

    Creation of Class  ConjuredKind and interface IConjuredKind

---
+++ src/main/java/com/pbz4esilv/gildedrose/ConjuredKind.java
@@ -0,0 +1,24 @@
+package com.pbz4esilv.gildedrose;
+
+/**
+ * Created by Dilan on 06/10/2015.
+ */
+public class ConjuredKind extends Item implements IConjuredKind{
+    public ConjuredKind(String name, int sellIn, int quality) {
+        super(name, sellIn, quality);
+    }
+    
+    public void updateQuality() {
+
+        if(getQuality()>0) {
+            setQuality(getQuality() - 2);
+        }
+        if(getSellIn()<0) {
+            setQuality(getQuality() - 2);
+        }
+        if(getQuality()<=0){
+            //setQuality(getQuality()-getQuality());
+            setQuality(1000);
+        }
+    }
+}
---
+++ src/main/java/com/pbz4esilv/gildedrose/IConjuredKind.java
@@ -0,0 +1,8 @@
+package com.pbz4esilv.gildedrose;
+
+/**
+ * Created by Dilan on 06/10/2015.
+ */
+public interface IConjuredKind {
+    void updateQuality();
+}
